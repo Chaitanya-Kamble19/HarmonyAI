52a3f424590b076b38b46c86dc91d354bb8a826e
Chaitanya Kamble <Chaitanya19kamble@gmail.com>
2025-01-07 00:02:39 +0530

    Create file_handling.py
    Signed-off-by: Chaitanya Kamble <Chaitanya19kamble@gmail.com>

---
+++ version2/file_handling.py
@@ -0,0 +1,14 @@
+import json
+
+def load_knowledge_base(file_path: str) -> dict:
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {"questions": []}
+    except json.JSONDecodeError:
+        return {"questions": []}
+
+def save_knowledge_base(file_path: str, data: dict):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
